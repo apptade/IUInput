50d788b1df4e93a96b7cafe9d6c659282d5c34c6
apptade <apptade64@gmail.com>
2025-04-07 16:04:13 +0300

    Update the basic input predicate system

--- SFInput.Runtime/Base/Managements/Predicates/IInputPredicateManager.cs
+++ SFInput.Runtime/Base/Managements/Predicates/IInputPredicateManager.cs
@@ -1,6 +1,13 @@
+using System;
+
 namespace SFInput {
 public interface IInputPredicateManager
 {
+    event Action<IInputPredicateManager> ManagerAdded;
+    event Action<IInputPredicateManager> ManagerRemoved;
+    event Action<IInputPredicate> PredicateAdded;
+    event Action<IInputPredicate> PredicateRemoved;
+
     void AddManager(IInputPredicateManager manager);
     void AddPredicate(IInputPredicate predicate);
 
