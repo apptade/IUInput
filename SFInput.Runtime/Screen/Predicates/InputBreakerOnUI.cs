69fb13d4bfe66df113bc1ffd7cba82c2e8020ba5
apptade <apptade64@gmail.com>
2025-04-22 20:14:32 +0300

    Update Input breaker on UI for screen

--- SFInput.Runtime/Screen/Predicates/InputBreakerOnUI.cs
+++ SFInput.Runtime/Screen/Predicates/InputBreakerOnUI.cs
@@ -1,68 +1,91 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.EventSystems;
 using System.Collections.Generic;
 
 namespace SFInput.Screen {
 public sealed class InputBreakerOnUI : MonoBehaviour
 {
-    [SerializeField] private InputManager<MovementInputController, MovementInputData> _addableMovementManager;
+    [SerializeField] private InputManager<ClickInputController, ClickInputData> _clickManager;
+    [SerializeField] private InputManager<MovementInputController, MovementInputData> _movementManager;
+    [SerializeField] private InputManager<PinchInputController, PinchInputData> _pinchManager;
     [SerializeField] private EventSystem _eventSystem;
 
-    private IInputPredicate[] _inputPredicates;
-    public int MaxPointersCount { get => 10; }
+    private UIClickInputPredicate _clickInputPredicate;
+    private UIMovementInputPredicate _movementInputPredicate;
+    private UIPinchInputPredicate _pinchInputPredicate;
 
     private void Awake()
     {
-        _inputPredicates = new IInputPredicate[MaxPointersCount];
-        for (int i = 0; i < MaxPointersCount; i++)
-        {
-            _inputPredicates[i] = new UIInputPredicate(i, _eventSystem);
-        }
+        _clickInputPredicate = new(_eventSystem);
+        _movementInputPredicate = new(_eventSystem);
+        _pinchInputPredicate = new(_eventSystem);
     }
 
     private void OnEnable()
     {
-        //_addableMovementManager.ControllerManager.AddPredicates(_inputPredicates);
+        _clickManager.ControllerManager.PredicateManager.AddPredicate(_clickInputPredicate);
+        _movementManager.ControllerManager.PredicateManager.AddPredicate(_movementInputPredicate);
+        _pinchManager.ControllerManager.PredicateManager.AddPredicate(_pinchInputPredicate);
     }
 
     private void OnDisable()
     {
-        //_addableMovementManager.ControllerManager.RemovePredicates(_inputPredicates);
+        _clickManager.ControllerManager.PredicateManager.RemovePredicate(_clickInputPredicate);
+        _movementManager.ControllerManager.PredicateManager.RemovePredicate(_movementInputPredicate);
+        _pinchManager.ControllerManager.PredicateManager.RemovePredicate(_pinchInputPredicate);
     }
 
-    private sealed class UIInputPredicate : IInputPredicate
+    private abstract class UIInputPredicate<T> : IInputPredicate<T>
     {
-        private readonly int _pointerId;
         private readonly EventSystem _eventSystem;
         private readonly PointerEventData _memoryPointerEventData;
         private readonly List<RaycastResult> _memoryRaycastResult;
 
-        public UIInputPredicate(int pointerId, EventSystem eventSystem)
+        public UIInputPredicate(EventSystem eventSystem)
         {
-            _pointerId = pointerId;
             _eventSystem = eventSystem;
             _memoryPointerEventData = new(eventSystem);
             _memoryRaycastResult = new();
         }
 
-        public bool Result()
-        {
-            var mouseResult = true;
-            var touchResult = true;
+        public abstract bool Result(T entry);
 
-            if (Mouse.current != null) mouseResult = IsPositionNotOverUI(Mouse.current.position.value);
-            if (Touchscreen.current != null) touchResult = IsPositionNotOverUI(Touchscreen.current.touches[_pointerId].position.value);
-
-            return mouseResult && touchResult;
-        }
-
-        private bool IsPositionNotOverUI(in Vector2 position)
+        protected bool IsPositionNotOverUI(in Vector2 position)
         {
             _memoryPointerEventData.position = position;
             _eventSystem.RaycastAll(_memoryPointerEventData, _memoryRaycastResult);
 
             return _memoryRaycastResult.Count is 0;
+        }
+    }
+
+    private sealed class UIClickInputPredicate : UIInputPredicate<ClickInputController>
+    {
+        public UIClickInputPredicate(EventSystem eventSystem) : base(eventSystem){}
+
+        public override bool Result(ClickInputController entry)
+        {
+            return IsPositionNotOverUI(entry.SettableClickDownPosition);
+        }
+    }
+
+    private sealed class UIMovementInputPredicate : UIInputPredicate<MovementInputController>
+    {
+        public UIMovementInputPredicate(EventSystem eventSystem) : base(eventSystem){}
+
+        public override bool Result(MovementInputController entry)
+        {
+            return IsPositionNotOverUI(entry.SettablePosition);
+        }
+    }
+
+    private sealed class UIPinchInputPredicate : UIInputPredicate<PinchInputController>
+    {
+        public UIPinchInputPredicate(EventSystem eventSystem) : base(eventSystem){}
+
+        public override bool Result(PinchInputController entry)
+        {
+            return IsPositionNotOverUI(entry.SettableMiddlePosition);
         }
     }
 }}
